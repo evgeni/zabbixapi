c38626fc2495f3aeba2e689fd349bd17c98c6edf
Evgeni Golov <evgeni@golov.de>
2023-12-30 11:28:14 +0100

    valuemaps need a hostid since 5.4

--- spec/acceptance/valuemap_spec.rb
+++ spec/acceptance/valuemap_spec.rb
@@ -3,18 +3,30 @@ require 'spec_helper'
 describe 'valuemap' do
   before :all do
     @valuemap = gen_name 'valuemap'
+    @hostgroup = gen_name 'hostgroup'
+    @hostgroupid = zbx.hostgroups.create(name: @hostgroup)
+    @template = gen_name 'template'
+    @templateid = zbx.templates.create(
+      host: @template,
+      groups: [groupid: @hostgroupid]
+    )
   end
 
   context 'when not exists' do
     describe 'create' do
       it 'should return an integer id' do
-        valuemapid = zbx.valuemaps.create_or_update(
+        params = {
           name: @valuemap,
           mappings: [
             'newvalue' => 'test',
             'value' => 'test'
           ]
-        )
+        }
+        if Gem::Version.new(zbx.client.api_version) >= VALUEMAP_HOSTID_REQUIRED_VERSION
+          params[:hostid] = @templateid
+        end
+
+        valuemapid = zbx.valuemaps.create_or_update(**params)
         expect(valuemapid).to be_kind_of(Integer)
       end
     end
@@ -22,25 +34,23 @@ describe 'valuemap' do
 
   context 'when exists' do
     before do
-      @valuemapid = zbx.valuemaps.create_or_update(
+      @params = {
         name: @valuemap,
         mappings: [
           'newvalue' => 'test',
           'value' => 'test'
         ]
-      )
+      }
+      if Gem::Version.new(zbx.client.api_version) >= VALUEMAP_HOSTID_REQUIRED_VERSION
+        @params[:hostid] = @templateid
+      end
+      @valuemapid = zbx.valuemaps.create_or_update(**@params)
     end
 
     describe 'create_or_update' do
       it 'should return id' do
         expect(
-          zbx.valuemaps.create_or_update(
-            name: @valuemap,
-            mappings: [
-              'newvalue' => 'test',
-              'value' => 'test'
-            ]
-          )
+          zbx.valuemaps.create_or_update(**@params)
         ).to eq @valuemapid
       end
 
